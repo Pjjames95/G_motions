165c3b930838bc13d628cd61eddff75889e9b573
Gachombaj <gachombajames7@gmail.com>
2025-01-31 14:05:20 +0300

    updated on the settings

--- G_motionscommerce/settings.py
+++ G_motionscommerce/settings.py
@@ -83,22 +83,22 @@ WSGI_APPLICATION = 'G_motionscommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-    "default": {
-        "ENGINE": 'django.db.backends.postgresql',
-        "NAME": "g_motions",
-        "USER": "g_motions_user",
-        "PASSWORD": "E3ohbqZuMTdu1UucUvSKXtyugYFdQQJ9",
-        "HOST": "dpg-cu7olgdsvqrc739aav2g-a.oregon-postgres.render.com",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": 'django.db.backends.postgresql',
+#         "NAME": "g_motions",
+#         "USER": "g_motions_user",
+#         "PASSWORD": "E3ohbqZuMTdu1UucUvSKXtyugYFdQQJ9",
+#         "HOST": "dpg-cu7olgdsvqrc739aav2g-a.oregon-postgres.render.com",
+#         "PORT": "5432",
+#     }
+# }
 
 
 #email credentials
